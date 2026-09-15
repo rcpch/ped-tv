15ce5c4802cd0be657b6f66ba2a87a5d779eb878
Michael Barton <michael.barton@rcpch.ac.uk>
2026-09-15 13:45:59 +0100

    Fix trailing space oopsie

--- pedtv/settings.py
+++ pedtv/settings.py
@@ -13,8 +13,8 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
-CSRF_TRUSTED_ORIGINS  = os.environ.get("CSRF_TRUSTED_ORIGINS ", "http://localhost,http://127.0.0.1").split(",")
+ALLOWED_HOSTS = [h.strip() for h in os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")]
+CSRF_TRUSTED_ORIGINS = [o.strip() for o in os.environ.get("CSRF_TRUSTED_ORIGINS", "http://localhost,http://127.0.0.1").split(",")]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
